f3bcefb7cbd9f76a6ed504bd1c39edb01b5dc0e9
Federico Grennon <fgrennon@steplix.com>
2023-04-19 17:25:13 -0300

    Fixing get tag ingo

--- dist/index.js
+++ dist/index.js
@@ -10899,12 +10899,10 @@ exports.getTagInfo = async (tag) => {
         process.exit(tagSha.exitCode);
     }
 
-    console.log(`Searching tag sha ${tagSha.stdout}`);
-
     const octokit = getOctokit(githubToken);
     const response = await octokit.rest.git.getCommit({
         ...context.repo,
-        commit_sha: tagSha.stdout
+        commit_sha: tagSha.stdout.trim()
     });
 
     return {
